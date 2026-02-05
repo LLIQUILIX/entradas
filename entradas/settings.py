992b5d555cb9d764c18fbd4e1037d7a15e0d3a15
Stevan Lliquin <lliquin18@hotmail.com>
2026-02-05 15:22:18 -0500

    Configuramos static y media para produccion

--- entradas/settings.py
+++ entradas/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL='/media/'
-MEDIA_ROOT=(os.path.join(BASE_DIR,'entradas/media'))
+MEDIA_ROOT= BASE_DIR / 'media'
 
 
 # envio de correo
