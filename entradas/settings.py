5d9d03bb5d1962fa933016ae300a694e0a73eb7f
Stevan Lliquin <lliquin18@hotmail.com>
2026-02-04 20:03:21 -0500

    Configuramos el Debug y Allowed_Hosts para produccion

--- entradas/settings.py
+++ entradas/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-15+l)k*9%l8g0om#t3@^8&z9-n0nomuen+ah!z#6blh=n8w38&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['LLIQUILIX.pythonanywhere.com']
+
 
 
 # Application definition
@@ -137,3 +138,4 @@ EMAIL_HOST_PASSWORD = 'dvmz kjfo wwcw csbm'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'login'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
